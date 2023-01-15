a1b649a4b857fda93e1dc5609a909273295f1075
MarioVazquez01 <mvazquez52@alumnos.uaq.mx>
2023-01-14 19:49:33 -0600

    e

---
+++ Identificador.R
@@ -0,0 +1,19 @@
+identificar_q <- function(){
+  a <- readline(prompt = "Prueba a")
+  b <- readline(prompt = "Prueba b")
+  c <- readline(prompt = "Prueba c")
+  d <- readline(prompt = "Prueba d")
+  e <- readline(prompt = "Prueba e")
+  
+  matriz <- as.matrix (a, b, c, d, e, nrow=1)
+}
+
+
+
+identificar_q()
+
+a <-0
+b<-0
+c <- 0
+d <- 0
+e <- 0
